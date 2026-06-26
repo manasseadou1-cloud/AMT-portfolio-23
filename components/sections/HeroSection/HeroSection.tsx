4eeb9c330582e837b4e2aa6ce144420f149fb61d
manasseadou1-cloud <manasseadou1@gmail.com>
2026-06-26 20:37:30 +0000

    change profil

--- components/sections/HeroSection/HeroSection.tsx
+++ components/sections/HeroSection/HeroSection.tsx
@@ -126,7 +126,7 @@ export function HeroSection() {
                    Copiez votre photo dans le dossier public/ du projet
                 ====================================================== */}
             <img
-              src="/maes.jpeg"
+              src="/Prophoto.jpeg"
               alt={`Photo de ${profile.firstName}`}
               className={styles.profilePhoto}
             />
